61202935e7181b12e0f158f2969aaf646e3dcd5b
Anbukkarasan <anbukkarasan2004@gmail.com>
2026-03-30 11:02:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'alstom-moc-secret-key-2024'
 
 app.config['SECRET_KEY'] = 'alstom-moc-secret-key-2024'
-app.config['SQLALCHEMY_DATABASE_URI']="postgresql://postgres:Anbu%402803200@aws-0-ap-south-1.pooler.supabase.com:6543/postgres"
+app.config['SQLALCHEMY_DATABASE_URI']="postgresql://postgres:Anbu%402803200@db.hqgosnkmtlpbxneegiph.supabase.co:5432/postgres"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
