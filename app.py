1b2feb5ae5529ff0387089990206869c7cfac455
Anbukkarasan <anbukkarasan2004@gmail.com>
2026-03-30 09:28:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'alstom-moc-secret-key-2024'
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://USER:PASSWORD@HOST:PORT/DATABASE"
+app.config['SQLALCHEMY_DATABASE_URI'] = "SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Anbu@2803200@db.hqgosnkmtlpbxneegiph.supabase.co:5432/postgres"E"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {"connect_args": {"check_same_thread": True}}
 
